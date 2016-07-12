11905bd28021192d5f4ab2a83b32f7d0dbc9fab9
Guillermo <g.puente.allott@gmail.com>
2016-07-12 20:32:44 +0200

    implementations uses an internal function to turn image into inverse

--- goinverse.go
+++ goinverse.go
@@ -9,53 +9,79 @@ import "image/png" // register the PNG format with the image package
 func main() {
 
 	args := os.Args[1:]
+
+	// validation of arguments
+	if len(args) < 1 {
+		fmt.Printf("goinverse. argument input file missing\n")
+		os.Exit(1)
+	}
+	if len(args) < 2 {
+		fmt.Printf("goinverse. argument output file missing\n")
+		os.Exit(1)
+	}
+
 	input := args[0]
 	output := args[1]
 
-    fmt.Printf("goinverse. Input=%q Output=%q\n", input, output )
+	fmt.Printf("goinverse. input=%q output=%q\n", input, output)
 
-    infile, err := os.Open( input )
-    if err != nil {
-        // replace this with real error handling
-        panic(err)
-    }
-    defer infile.Close()
+	infile, err := os.Open(input)
+	if err != nil {
+		// replace this with real error handling
+		panic(err)
+	}
+	defer infile.Close()
 
-    // Decode will figure out what type of image is in the file on its own.
-    // We just have to be sure all the image packages we want are imported.
-    src, _, err := image.Decode(infile)
-    if err != nil {
-        // replace this with real error handling
-        panic(err)
-    }
+	// Decode will figure out what type of image is in the file on its own.
+	// We just have to be sure all the image packages we want are imported.
+	srcImage, _, err := image.Decode(infile)
+	if err != nil {
+		// replace this with real error handling
+		panic(err)
+	}
 
-    // Create a new grayscale image
-    bounds := src.Bounds()
-    w, h := bounds.Max.X, bounds.Max.Y
-    rectangle := image.Rect(0, 0, w, h)
-    grayImage := image.NewGray( rectangle )
-    for x := 0; x < w; x++ {
-        for y := 0; y < h; y++ {
+	newImage := InvertImage(srcImage)
 
-            oldColor := src.At(x, y)
-            red, green, blue, alpha := oldColor.RGBA()
+	// Encode the new image to the output file
+	outfile, err := os.Create(output)
+	if err != nil {
+		// replace this with real error handling
+		panic(err)
+	}
+	defer outfile.Close()
 
+	png.Encode(outfile, newImage)
 
-            averaged := uint8( ( (red + green + blue) / 3 ) >> 8 )
-            grayColor := color.RGBA{ averaged, averaged, averaged, uint8(alpha>>8) }
+	fmt.Printf("goinverse. done\n")
+}
 
-            // grayColor := color.GrayModel.Convert(oldColor)
+/**
+ * Returns an inverted image.Image from the given one.
+ * Alpha channel is preserved.
+ */
+func InvertImage(srcImage image.Image) image.Image {
 
-            grayImage.Set(x, y, grayColor)
-        }
-    }
+	// Create a new image
+	bounds := srcImage.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
+	rectangle := image.Rect(0, 0, w, h)
+	newImage := image.NewRGBA(rectangle)
 
-    // Encode the grayscale image to the output file
-    outfile, err := os.Create( output )
-    if err != nil {
-        // replace this with real error handling
-        panic(err)
-    }
-    defer outfile.Close()
-    png.Encode(outfile, grayImage)
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+
+			oldColor := srcImage.At(x, y)
+			red, green, blue, alpha := oldColor.RGBA()
+
+			// transforming pixel into inverse.
+			ired := 255 - (uint8(red >> 8))
+			igreen := 255 - (uint8(green >> 8))
+			iblue := 255 - (uint8(blue >> 8))
+			newColor := color.RGBA{ired, igreen, iblue, uint8(alpha >> 8)}
+
+			newImage.Set(x, y, newColor)
+		}
+	}
+
+	return newImage
 }
